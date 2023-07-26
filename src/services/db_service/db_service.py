e736f2938d30ea022799b83b23c0594041518ac4
Ivan Baluk <ivan.baluk@bk.ru>
2023-07-26 16:47:15 +0200

    add Repo_psql realization

--- src/services/db_service/db_service.py
+++ src/services/db_service/db_service.py
@@ -5,22 +5,75 @@ from abc import ABC, abstractmethod
 
 
 class Repo_interface(ABC):
+	# TODO: add DTO signatures
 	@abstractmethod
-	async def get_orders(self, /, ): pass
-
-	@abstractmethod
-	async def new_order(self, /, id_: str): pass
+	async def init(self) -> None: pass
 
 	@abstractmethod
-	async def get_subs(self, /, ): pass
+	async def get_orders(self, /) -> tuple[int, ...]: pass
 
 	@abstractmethod
-	async def new_sub(self, /, id_: str): pass
+	async def new_order(self, /, id_: int) -> None: pass
 
 	@abstractmethod
-	async def del_sub(self, /, id_: str): pass
+	async def get_subs(self, /) -> tuple[str, ...]: pass
+
+	@abstractmethod
+	async def new_sub(self, /, id_: str) -> None: pass
+
+	@abstractmethod
+	async def del_sub(self, /, id_: str) -> None: pass
 
 
+class Repo_psql(Repo_interface):
+	conn: Connection = None
+
+
+	def __init__(self, conn):
+		self.conn = conn
+
+
+	async def init(self) -> None:
+		await self.conn.execute("""
+			CREATE TABLE IF NOT EXISTS subs (
+				id varchar(25) PRIMARY KEY
+			);
+			CREATE TABLE IF NOT EXISTS orders (
+				id INT PRIMARY KEY
+			);
+		""")
+
+
+	async def get_orders(self, /) -> list[dict['id': int], ...]:
+		return await self.conn.fetch("""
+			SELECT * FROM orders;
+		""")
+
+
+	async def new_order(self, /, id_: int) -> None:
+		await self.conn.execute("""
+			INSERT INTO orders
+			VALUES ($1);
+		""", id_)
+
+
+	async def get_subs(self, /) -> tuple[str, ...]:
+		return await self.conn.fetch("""
+			SELECT * FROM subs;
+		""")
+
+
+	async def new_sub(self, /, id_: str) -> None:
+		await self.conn.execute("""
+			INSERT INTO subs
+			VALUES ($1);
+		""", id_)
+
+	async def del_sub(self, /, id_: str) -> None:
+		await self.conn.execute("""
+			DELETE FROM subs
+			WHERE id = $1;
+		""", id_)
 
 
 def get_orders():
@@ -55,3 +108,5 @@ def del_sub(id_: str):
 		for line in lines:
 			if line.strip() != id_:
 				db_file.write(line)
+
+

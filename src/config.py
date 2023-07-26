f3b8567d0ee5b0474494392bf692389154e4f0ff
Ivan Baluk <ivan.baluk@bk.ru>
2023-07-26 14:42:28 +0200

    update config: add database info to config

--- src/config.py
+++ src/config.py
@@ -2,21 +2,39 @@ import configparser
 from dataclasses import dataclass
 
 
+
+
 @dataclass
 class BotConfig:
     token: str
 
 
 @dataclass
+class DbConfig:
+    user: str
+    password: str
+    database: str
+    host: str
+
+
+@dataclass
 class Config:
     bot: BotConfig
+    db: DbConfig
 
 
 def load_config(path: str):
     config = configparser.ConfigParser()
     config.read(path)
-    print(config)
 
     return Config(
         bot=BotConfig(**config["bot"]),
+        db=DbConfig(**config["database"]),
     )
+
+
+if __name__=='__main__':
+    config = load_config('config.ini')
+
+    from pprint import pprint
+    pprint(config)

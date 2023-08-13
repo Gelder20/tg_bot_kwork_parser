32331de8ff9953bcd086a5f7237a5ca398b8a664
Ivan Baluk <ivan.baluk@bk.ru>
2023-08-14 00:00:51 +0300

    update requirements, minor changes, bug fix in sync_wrapper

--- src/app/logger/logger.py
+++ src/app/logger/logger.py
@@ -1,7 +1,6 @@
 import logging
 import traceback
 
-
 from inspect import iscoroutinefunction, isgeneratorfunction
 from types import TracebackType
 
@@ -11,84 +10,77 @@ from rich.logging import RichHandler
 
 
 logging.basicConfig(
-	level=logging.DEBUG,
-	format="%(message)s",
-	datefmt='[%Y-%m-%d %H:%M:%S]',
-	handlers=[RichHandler(rich_tracebacks=True)]
+    level=logging.DEBUG,
+    format="%(message)s",
+    datefmt='[%Y-%m-%d %H:%M:%S]',
+    handlers=[RichHandler(rich_tracebacks=True)]
 )
 
 
 class MyLogger(logging.getLoggerClass()):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
 
-		self.catch._logger = self
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+        self.catch._logger = self
 
-	class catch:
-		_logger = None
+    class catch:
+        _logger = None
 
-		msg = None
-		func = None
-		exc = None
+        msg = None
+        func = None
+        exc = None
 
+        def __init__(self, msg="An error in func '{name}':", *, reraise=False, exc=Exception):
+            self.msg = msg
+            self.func = None
+            self.reraise = reraise
+            self.exc = exc
 
-		def __init__(self, msg="An error in func '{name}':", *, reraise=False, exc=Exception):
-			self.msg = msg
-			self.func = None
-			self.reraise = reraise
-			self.exc = exc
+        async def __async_wrapper(self, *args, **kwargs):
+            with self:
+                return await self.func(*args, **kwargs)
 
+        def __gen_wrapper(self, *args, **kwargs):
+            with self:
+                return (yield from self.func(*args, **kwargs))
 
-		async def __async_wrapper(self, *args, **kwargs):
-			with self:
-				return await self.func(*args, **kwargs)
+        def __sync_wrapper(self, *args, **kwargs):
+            with self:
+                return self.func(*args, **kwargs)
 
-		def __gen_wrapper(self, *args, **kwargs):
-			with self:
-				return (yield from self.func(*args, **kwargs))
+        def __call__(self, func):
+            self.func = func
 
-		def __sync_wrapper(self, *args, **kwargs):
-			with self:
-				return func(*args, **kwargs)
+            if iscoroutinefunction(func):
+                return self.__async_wrapper
 
+            if isgeneratorfunction(func):
+                return self.__gen_wrapper
 
-		def __call__(self, func):
-			self.func = func
+            return self.__sync_wrapper
 
+        def __enter__(self):
+            return None
 
-			if iscoroutinefunction(func):
-				return self.__async_wrapper
+        def __exit__(self, type_, value, traceback_):
+            if type_ is None: return
+            if not isinstance(value, self.exc): return
 
-			if isgeneratorfunction(func):
-				return self.__gen_wrapper
+            tb = None
+            frames = tuple(traceback.walk_tb(traceback_))
+            stop = len(frames) - 1
 
-			return self.__sync_wrapper
+            for i, (frame, _) in enumerate(reversed(frames)):
+                if i == stop: break
 
+                tb = TracebackType(tb, frame, frame.f_lasti, frame.f_lineno)
 
-		def __enter__(self):
-			return None
+            value.with_traceback(tb)
 
+            self._logger.exception(self.msg.format(name=self.func.__name__))
 
-		def __exit__(self, type_, value, traceback_):
-			if type_ is None: return
-			if not isinstance(value, self.exc): return
-
-			tb = None
-			frames = tuple(traceback.walk_tb(traceback_))
-			stop = len(frames) - 1
-
-			for i, (frame, _) in enumerate(reversed(frames)):
-				if i == stop: break
-
-				tb = TracebackType(tb, frame, frame.f_lasti, frame.f_lineno)
-
-			value.with_traceback(tb)
-
-			self._logger.exception(self.msg.format(name=self.func.__name__))
-
-			return not self.reraise
-
+            return not self.reraise
 
 
 logging.setLoggerClass(MyLogger)
